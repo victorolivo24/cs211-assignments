bd7098daee49f4caedf9747e5e80bb8059676e64
AFE123x <arunf400@gmail.com>
2025-02-03 15:20:40 -0500

    COMMIT FEB 3 @ 15:20
    - Updated sample c file

--- sample/first.c
+++ sample/first.c
@@ -1,12 +1,17 @@
 #include<stdio.h>
 #include<stdlib.h>
-int main(int argc, char** argv){
-  if(argc < 2) {
-    fprintf(stderr,"./first {text file}");
-    exit(1);
+
+/**
+  * This sample code is to help you get accustomed to basic C syntax,
+  * and how to read from text files.
+  **/
+int main(int argc, char** argv){ //similar to String[] args it java. argc is args.length, argv is the args array itself.
+  if(argc < 2) { //checks if there are 2 arguments!
+    fprintf(stderr,"./first {text file}"); //prints messages to stderr. Primarily for error messages
+    exit(1); //exits the program with a return code 1.
   }
 
-  FILE* file = fopen(argv[1],"r");
+  FILE* file = fopen(argv[1],"r"); //this will open a file specified by the user.
 
   if(file == NULL){ //if fopen fails to open the file, it returns NULL
     fprintf(stderr,"%s failed to open",argv[1]);
@@ -15,7 +20,7 @@ int main(int argc, char** argv){
 
   int x, y;
 
-  while(fscanf(file,"%d %d",&x, &y) == 2){
+  while(fscanf(file,"%d %d",&x, &y) == 2){ //attempts to read two integers, and stores them in x and y. fscanf returns the number of items successfully read.
     printf("%d + %d = %d\n",x,y,x+y);
   }
   return 0;
